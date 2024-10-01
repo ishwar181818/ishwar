6a9ff57870ba5500effaf6786621ba9541a3d19b
ishwar181818 <ishwarharbade55@gmail.com>
2024-10-01 23:59:27 +0530

    method has been added

--- GitHub/src/GitHubProjectgh/A.java
+++ GitHub/src/GitHubProjectgh/A.java
@@ -2,8 +2,16 @@ package GitHubProjectgh;
 
 public class A {
 	
+
+	public void m1()
+
 	
-	public static void main(String[] args) {
+	{
+		
+		System.out.println("Method m1 has been added");
+		
+	}	
+		public static void main(String[] args) {
 		
 		System.out.println("Welcome To GitHub");
 		
